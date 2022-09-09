dd07f690a18a579ebdb1def3c50d7395407ea872
Olamite <olamite17@gmail.com>
2022-09-09 09:45:32 -0700

    Life tough

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -21,6 +21,7 @@ if (n > 0)
 	printf("%i is positive\n", n);
 }
 else if (n == 0)
+{	
 	printf("%i is zero\n", n);
 }
 else if (n < 0)
@@ -28,3 +29,4 @@ else if (n < 0)
 	printf("%i is negative\n", n);
 }
 return (0);
+}
