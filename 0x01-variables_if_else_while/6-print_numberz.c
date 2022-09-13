7372b5af463d39a46e27b10f8bfbea3a9135fa9b
Olamite <olamite17@gmail.com>
2022-09-12 18:27:33 -0700

    Print numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - print -0123456789 using putchar
+* Return: Always 0 (Success)
+*
+*/
+
+int main(void)
+{
+	for let(int n = 0; n < 10; n++)
+{
+	putchar(n);
+}
+	putchar('\n');
+
+	return (0);
+
+}
