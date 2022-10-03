53bac637aa86843305788ebd737fb16a221f4d3d
Olamite <olamite17@gmail.com>
2022-10-02 19:53:41 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -7,7 +7,7 @@
  * @argc: The number of arguments passed to the program.
  * @argv: An array of pointers to the arguments.
  *
- * Return: If one of the numners contains symbols that are non-digits - 1.
+ * Return: If one of the numbers contains symbols that are non-digits - 1.
  * 		Otherwise - 0.
  */
 
@@ -15,9 +15,9 @@ int main(int argc, char *argv[])
 {
 	int num, digit, sum = 0;
 
-	for (num = 1 num < argcl num++)
+	for (num = 1; num < argc; num++)
 	{
-		for (digit = 0 argv[num][digit]; digit++)
+		for (digit = 0; argv[num][digit]; digit++)
 		{
 			if (argv[num][digit] < '0' || argv[num][digit] > '9')
 			{
